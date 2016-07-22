87fc927bef8afdd45f8e0cf55a0db453b7f39b63
zhun <zhun@huoli.com>
2016-07-22 09:31:14 +0800

    async document.write will overwrite the document

--- demo/loadTest.js
+++ demo/loadTest.js
@@ -1,7 +1,9 @@
 (function () {
   //setTimeout(function() {
     alert('this is test');
-    document.write('<script type="text/javascript" charset="utf-8" id="gt_lib" src="./test1.js"></script>')
-  //}, 5000);
+    //setTimeout(function() {
+    //    document.write('<h1>rerw</h1>');
+    //}, 1000);
 
+    //document.write('<script type="text/javascript" charset="utf-8" id="gt_lib" src="./test1.js"></script>')
 })();
